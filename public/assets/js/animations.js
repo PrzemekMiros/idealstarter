f6f8023bc0b788af90a6dfb4e51a22258851091f
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-12-21 23:31:24 +0100

    Fixes

--- public/assets/js/animations.js
+++ public/assets/js/animations.js
@@ -1,6 +1,6 @@
 function animationMain() {
   gsap.registerPlugin(ScrollTrigger);
-
+/*
   const lenis = new Lenis({
   duration: 1.2,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
@@ -43,6 +43,7 @@ function animationMain() {
   gsap.ticker.add((time)=>{
     lenis.raf(time * 1000)
   });
+  */
   
   if (window.matchMedia("(min-width: 767px)").matches) {
 
