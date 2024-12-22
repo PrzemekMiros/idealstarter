19265ac0d4c99c4bd226fc547b2fa4d17caeb47d
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-12-22 18:14:31 +0100

    Add carousels

--- src/assets/js/animations.js
+++ src/assets/js/animations.js
@@ -1,13 +1,13 @@
 function animationMain() {
   gsap.registerPlugin(ScrollTrigger);
-
+/*
   const lenis = new Lenis({
-  duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  // duration: .2,
+  // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
   direction: "vertical",
   gestureDirection: "vertical",
-  lerp: 0.2,
-  smooth: 5,
+  lerp: 0.15,
+  smooth: true,
   smoothTouch: false,
   touchMultiplier: 2,
   wheelMultiplier: 1,
@@ -43,7 +43,7 @@ function animationMain() {
   gsap.ticker.add((time)=>{
     lenis.raf(time * 1000)
   });
-  
+*/
   
   if (window.matchMedia("(min-width: 767px)").matches) {
 
